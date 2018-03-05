2331a54031ca2e2501f9939f768f574d9149472e
Den Drobiazko <den.d@techery.io>
2018-03-05 18:46:37 +0200

    Implement Attribute field type check
    Primitives, String, Boolean and boxed numbers are applicable

--- service-compiler/src/main/java/io/techery/analytics/compiler/validator/AttributesValidator.java
+++ service-compiler/src/main/java/io/techery/analytics/compiler/validator/AttributesValidator.java
@@ -38,8 +38,15 @@ public class AttributesValidator implements Validator<AnalyticActionClass> {
       for (Element element : annotatedElements) {
 
          if (element.getKind() != ElementKind.FIELD) {
-            errors.add(new ValidationError(element, "Only fields can be annotated with %s! Please, see class %s",
+            errors.add(new ValidationError(element, "Only fields can be annotated with %s! Please see class %s",
                   annotationName, value.typeName.toString()));
+         }
+
+         if (!isBoolean(element) && !isNumber(element)
+               && !isString(element) && !isPrimitive(element)) {
+            errors.add(new ValidationError(element,
+                  "Only primitives, Boolean, String and Number fields can have %s annotation! Please see class %s",
+                    annotationName, value.typeName.toString()));
          }
 
          // TODO does not work correctly with kotlin classes - field is always private, need to check getters
@@ -50,5 +57,25 @@ public class AttributesValidator implements Validator<AnalyticActionClass> {
       }
 
       return errors;
+   }
+
+   private boolean isPrimitive(Element element) {
+      return element.asType().getKind().isPrimitive();
+   }
+
+   private boolean isBoolean(Element element) {
+      return element.asType().toString().equals(Boolean.class.getName());
+   }
+
+   private boolean isString(Element element) {
+      return element.asType().toString().equals(String.class.getName());
+   }
+
+   private boolean isNumber(Element element) {
+      final String elementName = element.asType().toString();
+      return elementName.equals(Integer.class.getName())
+            || elementName.equals(Float.class.getName())
+            || elementName.equals(Double.class.getName())
+            || elementName.equals(Long.class.getName());
    }
 }
